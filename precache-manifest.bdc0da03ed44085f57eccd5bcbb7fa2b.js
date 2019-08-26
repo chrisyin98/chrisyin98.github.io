01ec3f96ebc3ef05a3d8ad1400ad479ecd4c06da
chrisyin98 <chrisyin98@gmail.com>
2019-08-25 23:01:22 -0700

    Updates

---
+++ precache-manifest.bdc0da03ed44085f57eccd5bcbb7fa2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f5eafa783dcf3f554f9d88ec60d54a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c777a3b0dc3e4c33694b",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "ee5585d2b14e086ea766",
+    "url": "/static/css/main.3de1f8cf.chunk.css"
+  },
+  {
+    "revision": "c777a3b0dc3e4c33694b",
+    "url": "/static/js/2.fec7c787.chunk.js"
+  },
+  {
+    "revision": "ee5585d2b14e086ea766",
+    "url": "/static/js/main.c2461f87.chunk.js"
+  },
+  {
+    "revision": "0f9571fe81602c3b51b7",
+    "url": "/static/js/runtime~main.244a79c9.js"
+  }
+]);
